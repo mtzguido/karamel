5f68e7ea47ce2b87d64979f68df6609824d0261a
Guido Martínez <mtzguido@gmail.com>
2026-05-16 12:50:45 -0700

    krmllib old F*

--- krmllib/dist/generic/FStar_UInt_8_16_32_64.h
+++ krmllib/dist/generic/FStar_UInt_8_16_32_64.h
@@ -28,10 +28,6 @@ extern uint64_t FStar_UInt64_zero;
 
 extern uint64_t FStar_UInt64_one;
 
-extern uint64_t FStar_UInt64_rotate_right(uint64_t a, uint32_t s);
-
-extern uint64_t FStar_UInt64_rotate_left(uint64_t a, uint32_t s);
-
 extern bool FStar_UInt64_ne(uint64_t a, uint64_t b);
 
 extern uint64_t FStar_UInt64_minus(uint64_t a);
@@ -83,10 +79,6 @@ extern uint32_t FStar_UInt32_uint_to_t(krml_checked_int_t x);
 extern uint32_t FStar_UInt32_zero;
 
 extern uint32_t FStar_UInt32_one;
-
-extern uint32_t FStar_UInt32_rotate_right(uint32_t a, uint32_t s);
-
-extern uint32_t FStar_UInt32_rotate_left(uint32_t a, uint32_t s);
 
 extern bool FStar_UInt32_ne(uint32_t a, uint32_t b);
 
@@ -140,10 +132,6 @@ extern uint16_t FStar_UInt16_zero;
 
 extern uint16_t FStar_UInt16_one;
 
-extern uint16_t FStar_UInt16_rotate_right(uint16_t a, uint32_t s);
-
-extern uint16_t FStar_UInt16_rotate_left(uint16_t a, uint32_t s);
-
 extern bool FStar_UInt16_ne(uint16_t a, uint16_t b);
 
 extern uint16_t FStar_UInt16_minus(uint16_t a);
@@ -195,10 +183,6 @@ extern uint8_t FStar_UInt8_uint_to_t(krml_checked_int_t x);
 extern uint8_t FStar_UInt8_zero;
 
 extern uint8_t FStar_UInt8_one;
-
-extern uint8_t FStar_UInt8_rotate_right(uint8_t a, uint32_t s);
-
-extern uint8_t FStar_UInt8_rotate_left(uint8_t a, uint32_t s);
 
 extern bool FStar_UInt8_ne(uint8_t a, uint8_t b);
 
